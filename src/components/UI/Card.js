b12dadefffdd98e9e6376c76efd8f33739b0c691
Nandish Desai <nandish.desai@rtlsuper.com>
2023-01-18 22:45:41 -0500

    Complete Section 3

---
+++ src/components/UI/Card.js
@@ -0,0 +1,11 @@
+
+import './Card.css';
+
+const Card = (props) => {
+    const clasess = 'card ' + props.className;
+    return (
+        <div className={clasess}>{props.children}</div>
+    );
+}
+
+export default Card;
